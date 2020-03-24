5182b941bff2957556a5d8f78a51c57d21f206d2
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-24 13:07:34 +0530

    MODIFIED:Displayed start fresh with resetting the board

--- problems/ticTacToe.sh
+++ problems/ticTacToe.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 #DISPLAYE THE WELCOME STATEMENT
 echo "Welcome to the Tic Tac Toe Game"
+null=" "
+declare -a board
+board=(" " " " " " " " " " " " " " " " " ")
+function resultingBoard()
+{
+	echo "${board[0]} | ${board[1]} | ${board[2]}"
+	echo "----------"
+	echo "${board[3]} | ${board[4]} | ${board[5]}"
+	echo "----------"
+	echo "${board[6]} | ${board[7]} | ${board[6]}"
+}
+#CALL THE FUNCTION
+resultingBoard
