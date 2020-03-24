a55d0b5f1d6ff5222b8a8905ccde9bf1887dce4c
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-25 02:54:27 +0530

    MODIFIED:Displayed winner tie and change the turn condition

--- problems/ticTacToe.sh
+++ problems/ticTacToe.sh
@@ -13,6 +13,7 @@ function displayBoard()
 	echo "----------"
 	echo "${board[6]} | ${board[7]} | ${board[8]}"
 }
+#ASSINING LETTER TO PLAYER AND COMPUTER
 function assiningLetter()
 {
 	#GENERATING RANDOM VALUE TO ASSIGN O OR X
@@ -32,7 +33,7 @@ function validCellNumber()
 {
    echo "Enter the valid number from 0 to 8 and choose position" 
    read data
-   if (($data>0 && $data<=8))
+   if (($data>=0 && $data<=8))
    then
       if [[ "${board[data]}"!=X || "${board[data]}"!=O ]]
       then
@@ -42,8 +43,10 @@ function validCellNumber()
 			validCellNumber
       fi
    fi
+
 displayBoard
 }
+#LOGIC FOR TO ASSIGN FIRST CHANCE
 function checkToss()
 {
 	#GENERATING RANDOM VALUE 0 FOR PALYER 1 FOR COMPUTER
@@ -58,7 +61,64 @@ function checkToss()
 	esac
 
 }
-displayBoard
 assiningLetter
+#LOGIC FOR WIN DROW AND NEXT CHANCE
+function gameOver()
+{
+	if [[ "${board[0]} ${board[1]} ${board[2]}" = "X X X" ||  "${board[0]} ${board[1]} ${board[2]}" = "O O O" ]]
+	then
+		echo result="Win" 
+	elif [[ "${board[3]} ${board[4]} ${board[5]}"="X X X"||"${board[3]} ${board[4]} ${board[5]}"="O O O " ]]
+	then
+		result="Win"
+	elif [[ "${board[6]} ${board[7]} ${board[8]}"="X X X"||"${board[6]} ${board[7]} ${board[8]}"="O O O " ]]
+	then
+		result="Win"
+	elif [[ "${board[0]} ${board[3]} ${board[6]}"="X X X"||"${board[0]} ${board[3]} ${board[6]}"="O O O " ]]
+	then		result="Win"
+	elif [[ "${board[1]} ${board[4]} ${board[7]}"="X X X"||"${board[1]} ${board[4]} ${board[7]}"="O O O " ]]
+	then
+		result="Win"
+	elif [[ "${board[2]} ${board[5]} ${board[8]}"="X X X"||"${board[2]} ${board[5]} ${board[8]}"="O O O " ]]
+	then
+		result="Win"
+	elif [[ "${board[0]} ${board[4]} ${board[8]}"="X X X"||"${board[0]} ${board[4]} ${board[8]}"="O O O " ]]
+	then
+		result="Win"
+	elif [[ "${board[6]} ${board[4]} ${board[2]}"="X X X"||"${board[6]} ${board[4]} ${board[2]}"="O O O " ]]
+ 	then
+		result="Win"
+	else
+		temp=0
+		for((index=0; index<${#board[@]}; index++))
+		do
+			if [[ "${board[$index]}"!=X || "${board[$index]}"!=O ]]
+			then
+				temp=1
+			fi
+		done
+			if(($temp==1))
+			then
+				result="Change"
+			else
+				result="Draw"
+			fi
+	fi
+			echo $result
+}
+while ((0==0))
+do
+displayBoard
 validCellNumber
-checkToss
+result="$(gameOver)"
+#echo "$result"
+done
+
+
+	
+
+
+
+
+
+
